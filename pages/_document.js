5bf31f45afb1dc824d81556b80d935fb29308c63
alejandrogsk <alejandrogsk9900@gmail.com>
2022-10-19 08:44:27 -0300

    ICO

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/web-icon.ico" />
+          <link rel="icon" href="/assets/logo-1.ico" />
           {/**Oswald & Poppins */}
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
