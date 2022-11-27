606ffdbe0b1d9275ba27730291188ba7f9f138ba
alejandrogsk <alejandrogsk9900@gmail.com>
2022-11-27 15:15:31 -0300

    Poppins test

--- pages/dupli/ecommerce.js
+++ pages/dupli/ecommerce.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import React from "react";
-import HeadComponent from "../components/global/HeadComponent";
-import Layout from "../components/global/Layout";
-import EcommerceEs from "../content/es/ecommerce.json";
-import CtaRegular from "../components/Services/CtaRegular";
-import Banner from "../components/Services/Banner";
+import HeadComponent from "../../components/global/HeadComponent";
+import Layout from "../../components/global/Layout";
+import EcommerceEs from "../../content/es/ecommerce.json";
+import CtaRegular from "../../components/Services/CtaRegular";
+import Banner from "../../components/Services/Banner";
 
 const ecommerces = ({data}) => {
     const { head, hero, banner, platforms, services, payments, contact } = data;
@@ -13,21 +13,22 @@ const ecommerces = ({data}) => {
       <HeadComponent title={head.title} description={head.description} />
       <Layout>
             <section className="min-h-screen px-8 md:px-26 lg:px-48 relative ">
-                <h1 className="pt-32 lg:pt-36 mb-12 text-3xl md:text-4xl lg:text-5xl	font-semibold uppercase">
-                    {hero.title}
+                <h1 className="pt-32 lg:pt-36 mb-12 text-3xl md:text-4xl lg:text-5xl	font-poppins font-normal ">
+                    
+                    Somos una agencia especializada en <span className="underline-custom font-poppins">Ecommerce</span>
                 </h1>
                 {hero.content.map((paragraph, i) => (
-                    <p className="mt-8 text-lg md:text-xl lg:text-2xl" key={i}>
+                    <p className="mt-8 text-lg md:text-xl lg:text-2xl font-light	" key={i}>
                         {paragraph}
                     </p>
                 ))}
-                <span className="ecommerce_hero-span--1 bg-yellow-500"></span>
+                
             </section>
 
             <Banner banner={banner.image1} />
           
             <section className="ecommerce_platforms bg-white px-6 md:px-12 lg:px-16 py-20 grid grid-cols-1 justify-items-center">
-                <h2 className="text-3xl md:text-4xl lg:text-5xl	font-bold uppercase  text-center mb-16 text-black w-full md:w-10/12 lg:w-8/12">{platforms.title}</h2>
+                <h2 className="text-3xl md:text-4xl lg:text-5xl	font-normal   text-center mb-16 text-black w-full md:w-10/12 lg:w-8/12 font-poppins">{platforms.title}</h2>
                 <div className="grid gap-8 grid-cols-1 md:grid-cols-3 justify-center align-center">
                 {
                     platforms.list.map((platform, i) => (
@@ -37,7 +38,7 @@ const ecommerces = ({data}) => {
                             width={platform.image.width} height={platform.image.height}
                             layout="intrinsic"
                         />
-                        <p className="text-lg lg:text-xl text-custom-black mt-6">{platform.content}</p>
+                        <p className="text-lg lg:text-xl font-light	 text-custom-black mt-6">{platform.content}</p>
                     </div>
                     ))
                 }
@@ -49,7 +50,7 @@ const ecommerces = ({data}) => {
 
 
             <section className="ecommerce_services px-6 md:px-12 lg:px-16 py-20">
-                <h2 className="text-3xl md:text-4xl lg:text-5xl	font-bold uppercase  text-center mb-16">{services.title}</h2>
+                <h2 className="text-3xl md:text-4xl lg:text-5xl	font-normal font-poppins  text-center mb-16">{services.title}</h2>
                 <div className="ecommerce_services-section grid grid-cols-1 gap-16">
                     {services.content.map((serviceList, i) => (
                         <div key={i} className="grid grid-cols-1 gap-8 lg:grid-cols-2 items-center justify-items-center ">
@@ -57,10 +58,10 @@ const ecommerces = ({data}) => {
                                 {serviceList.list.map((element, i) => (
 
                                     <div className="first:mt-0 mt-6" key={i}>
-                                        <h3 className="uppercase text-2xl font-semibold">
+                                        <h3 className=" text-2xl font-normal font-poppins">
                                             {element.title}
                                         </h3>
-                                        <p className="text-lg lg:text-xl mt-3">
+                                        <p className="text-lg lg:text-xl mt-3 font-light	">
                                             {element.subtitle}
                                         </p>
                                         
